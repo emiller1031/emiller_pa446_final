515a29d82cd7f0d74e9922535dcce3cd41f8ba37
Elizabeth Miller <e.e.miller1031@gmail.com>
2024-05-01 22:43:54 -0500

    done with pa446 :')

--- partIII/app.R
+++ partIII/app.R
@@ -1,60 +1,60 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(datasets)
 library(plotly)
+library(shinythemes)
+
 data <- as_tibble(iris)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel(textOutput("nameOutput")),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            textInput("name",
-                      "What is your name?"),
-            textInput("rows",
-                      "Number of rows to show in the table"),
-            selectizeInput("yaxis",
-                           "Y-Axis",
-                           choice = c("Sepal Length" = "Sepal.Length",
-                                      "Sepal Width" = "Sepal.Width",
-                                      "Petal Length" = "Petal.Length",
-                                      "Petal Width" = "Petal.Width",
-                                      "Species" = "Species")),
-            selectizeInput("xaxis",
-                           "X-Axis",
-                           choice = c("Sepal Length" = "Sepal.Length",
-                                      "Sepal Width" = "Sepal.Width",
-                                      "Petal Length" = "Petal.Length",
-                                      "Petal Width" = "Petal.Width",
-                                      "Species" = "Species")),
-        ),
-
-        # Show a plot of the generated graph
-        mainPanel(
-          tabsetPanel(
-            tabPanel("Plot", plotOutput("distPlot")),
-            tabPanel("Table", tableOutput("table"))
-        )
+  
+  # select theme
+  theme = shinytheme("united"),
+  
+  # title with greeting 
+  titlePanel(textOutput("nameOutput")),
+  
+  # sidebar with inputs and main panel with tabs
+  sidebarLayout(
+    sidebarPanel(
+      textInput("name", "What is your name?"),
+      textInput("rows", "Number of rows to show in the table"),
+      selectizeInput("yaxis", "Y-Axis", choice = c(
+        "Sepal Length" = "Sepal.Length",
+        "Sepal Width" = "Sepal.Width",
+        "Petal Length" = "Petal.Length",
+        "Petal Width" = "Petal.Width",
+        "Species" = "Species"
+      )),
+      selectizeInput("xaxis", "X-Axis", choice = c(
+        "Sepal Length" = "Sepal.Length",
+        "Sepal Width" = "Sepal.Width",
+        "Petal Length" = "Petal.Length",
+        "Petal Width" = "Petal.Width",
+        "Species" = "Species"
+      ))
+    ),
+    
+    # Main panel with tabs for plot and table
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Plot", plotlyOutput("irisPlot")),
+        tabPanel("Table", tableOutput("table"))
+      )
     )
-)
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$distPlot <- renderPlotly({
+  # Render the greeting message
+  output$nameOutput <- renderText({
+    paste("Hello", input$name)
+  })
+  
+  # Render the plot
+  output$irisPlot <- renderPlotly({
     irisgraph <- data %>%
       ggplot() +
       geom_point(aes_string(
@@ -62,14 +62,12 @@ server <- function(input, output) {
         y = input$yaxis)) +
       ggtitle("Iris Data Plot")
     ggplotly(irisgraph)
+    print(irisgraph)
   })
   
+  # Render the table
   output$table <- renderTable({
     head(data, n = as.numeric(input$rows))
-  })
-  
-  output$namOutput <- renderText({
-    paste("Hello", input$name)
   })
 }
 
